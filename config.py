7ad7beff9a0ca4266823c17cf557dbf9b0d5d956
xSXGx <silviuxgaming@gmail.com>
2024-04-17 19:40:34 +0300

    config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@
 #                        #
 ##########################
 
+
 from libqtile import bar, layout, qtile, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
